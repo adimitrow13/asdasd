2fff6331ebc728cc62d25a7df978c532633678c3
Alex Dimitrov <adimitrow@outlook.com>
2026-02-20 11:39:12 +0200

    валидация, логин в appdbcontext

--- asdasd/Data/ApplicationDbContext.cs
+++ asdasd/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using asdasd.Models;
 
 namespace asdasd.Data;
     
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options) { }
--- asdasd/Models/Product.cs
+++ asdasd/Models/Product.cs
@@ -1,8 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 namespace asdasd.Models;
 
 public class Product
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Наименованието е задължително.")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Името трябва да е между 3 и 100 символа.")]
+    [Display(Name = "Наименование")]
     public required string Name { get; set; }
+
+    [Required(ErrorMessage = "Цената е задължителна.")]
+    [Range(0.01, 100000, ErrorMessage = "Цената трябва да бъде между 0.01 и 100 000.")]
+    [DataType(DataType.Currency)]
+    [Display(Name = "Цена")]
     public decimal Price { get; set; }
 }
